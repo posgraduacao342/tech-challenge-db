431017058e570c83f1df92522ad6f12bb3ffffd4
Antonio Felype Moraes <49417747+antoniofmoraes@users.noreply.github.com>
2023-11-01 00:18:29 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,13 @@ provider "aws" {
 }
 
 variable "db_username" {
-  description = "Username for the database"
+  type      = string
+  sensitive = true
 }
 
 variable "db_password" {
-  description = "Password for the database"
+  type      = string
+  sensitive = true
 }
 
 resource "aws_db_instance" "postgresql_instance" {
