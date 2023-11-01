4ec59f312a33aba452c205a5ff3268ff20e643cc
Antonio Felype Moraes <49417747+antoniofmoraes@users.noreply.github.com>
2023-11-01 00:12:56 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ variable "db_password" {
 }
 
 resource "aws_db_instance" "postgresql_instance" {
-    allocated_storage    = 20
-    storage_type         = "gp2"
-    engine               = "postgres"
-    engine_version       = "13.1"
-    instance_class       = "db.t3.micro"
-    db_name              = "postgres"
-    username             = var.db_username
-    password             = var.db_password
+  allocated_storage = 20
+  storage_type      = "gp2"
+  engine            = "postgres"
+  engine_version    = "13.1"
+  instance_class    = "db.t3.micro"
+  db_name           = "postgres"
+  username          = var.db_username
+  password          = var.db_password
 }
